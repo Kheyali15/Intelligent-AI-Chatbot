a9d1a43121e185cb9bd4dc4e90949b5eb29f0c84
Kheyali15 <kheyalidhara15@gmail.com>
2023-09-10 13:41:02 +0530

    modifying

--- chatbot.py
+++ chatbot.py
@@ -48,12 +48,14 @@ def predict_class(sentence):
     return return_list
 
 def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if(i['tag']== tag):
-            result = random.choice(i['responses'])
-            break
+    if intents_list:
+        tag = intents_list[0]['intent']
+        list_of_intents = intents_json['intents']
+        for i in list_of_intents:
+            if i['tag'] == tag:
+                result = random.choice(i['responses'])
+                break
+
     return result
 
 print("GO! Bot is running!")
